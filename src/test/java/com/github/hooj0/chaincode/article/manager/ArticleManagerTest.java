f1c71498321191580cb38f73d97fa18de85378f3
hoojo <hoojo@qq.com>
2018-10-08 18:05:38 +0800

    :sparkles: :bento: :tada:  Init add ArticleManagerTest

---
+++ src/test/java/com/github/hooj0/chaincode/article/manager/ArticleManagerTest.java
@@ -0,0 +1,33 @@
+package com.github.hooj0.chaincode.article.manager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.github.hooj0.chaincode.deploy.article.configuration.DeployArticleConfiguration;
+import com.github.hooj0.chaincode.deploy.article.manager.ArticleManager;
+
+/**
+ * ArticleManagerTest
+ * @author hoojo
+ * @createDate 2018年8月13日 下午3:00:45
+ * @file ArticleManagerTest.java
+ * @blog http://hoojo.cnblogs.com
+ * @email hoojo_@126.com
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = DeployArticleConfiguration.class)
+public class ArticleManagerTest {
+
+	@Autowired
+	private ArticleManager manager;
+	
+	@Test
+	public void testInstall() throws Exception {
+		
+		System.out.println(manager.setup());
+	}
+}
