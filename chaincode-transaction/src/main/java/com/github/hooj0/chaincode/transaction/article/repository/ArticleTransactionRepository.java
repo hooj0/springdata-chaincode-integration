8b7ee9932b40a01ce9f80692a471c9548d121258
hoojo <hoojo@qq.com>
2020-04-11 17:28:20 +0800

    :sparkles: :bento: :recycle:  Changed article transaction repository

--- chaincode-transaction/src/main/java/com/github/hooj0/chaincode/transaction/article/repository/ArticleTransactionRepository.java
+++ chaincode-transaction/src/main/java/com/github/hooj0/chaincode/transaction/article/repository/ArticleTransactionRepository.java
@@ -22,7 +22,7 @@ import io.github.hooj0.springdata.fabric.chaincode.repository.ChaincodeRepositor
  * @email hoojo_@126.com
  * @version 1.0
  */
-@Chaincode(channel = "articlechannel", org = "peerOrg2", name = "article", type = Type.GO_LANG, version = "v0.1", path = "masget.com/article_cc")
+@Chaincode(channel = "articlechannel", org = "peerOrg2", name = "article", type = Type.GO_LANG, version = "v0.1", path = "hoojo.com/article_cc")
 public interface ArticleTransactionRepository extends ChaincodeRepository<ArticleEntity> {
 
 	@Invoke(func = "create")
